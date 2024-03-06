62701a840da56866aeb321487bd7572c074af896
BYEONGTAEK <respect21047436@gmail.com>
2024-03-06 20:44:21 +0900

    회원가입 미완

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,147 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>signup-page</div>;
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useToast } from "@/components/ui/use-toast";
+import { useForm } from "react-hook-form";
+import { registerSchema } from "@/vaildate/auth";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+type RegisterInput = z.infer<typeof registerSchema>;
+
+const Page = () => {
+  const { toast } = useToast();
+  const form = useForm<RegisterInput>({
+    //유효성검사
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      phone: "",
+      email: "",
+      role: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  // log the form data whenever it changes
+  console.log(form.watch());
+
+  // function onSubmit(values: z.infer<typeof registerSchema>) {
+  //   // Do something with the form values.
+  //   // ✅ This will be type-safe and validated.
+  //   console.log(values)
+  // }
+
+  function onSubmit(data: RegisterInput) {
+    const { password, confirmPassword } = data;
+    if (password !== confirmPassword) {
+      console.log("1234");
+      toast({
+        title: "비밀번호가 일치하지 않습니다.",
+        variant: "destructive",
+        duration: 1000,
+      });
+      return;
+    }
+    alert(JSON.stringify(data, null, 4));
+  }
+
+  return (
+    <Card className=" mx-auto my-[400px] w-[350px]">
+      <CardHeader>
+        <CardTitle>계정을 생성합니다</CardTitle>
+        <CardDescription>필수 정보를 입력해주세요.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            {/* Framer Motion  */}
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>이름</FormLabel>
+                  <FormControl>
+                    <Input placeholder="고길동" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>이메일</FormLabel>
+                  <FormControl>
+                    <Input placeholder="abc@def.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>비밀번호</FormLabel>
+                  <FormControl>
+                    <Input type={"password"} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>비밀번호 확인</FormLabel>
+                  <FormControl>
+                    <Input type={"password"} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
 };
 
-export default page;
+export default Page;
